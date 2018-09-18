4a9922d07b8fc5d6565339e28c7bcdaaa5e4999d
otaviosumi <otaviosumi@hotmail.com>
2018-09-18 21:53:28 +0000

    Lendo nome do input

--- encode.c
+++ encode.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char **argv){
 
 	int i;
-
-	printf("Entradas:\n");
+	char *filename;
 
 	for(i = 1; i < argc; i++){
 		if(argv[i][0] == '-'){
@@ -19,6 +19,12 @@ int main(int argc, char **argv){
 				printf("\nFlag não reconhecida, utilize: \n<-d> (codificação por diferenças)\n<-c> (codificação por carreira)\n<-h> (codificação Huffman)\n\n");
 			}
 		}
+		if(i == argc-1){
+			filename = argv[i];
+			printf("%s\n", filename);
+		}
 	}
+
+	//int status = system("./wave_reader Engine-Noise.wav");
 	
 }
