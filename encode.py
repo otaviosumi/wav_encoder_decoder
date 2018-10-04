14eb692b503a0020d027d8a1c8834cda1e28da51
otaviosumi <otaviosumi@hotmail.com>
2018-10-04 15:17:14 -0300

    compressao por diferenças funcionando

--- encode.py
+++ encode.py
@@ -2,23 +2,38 @@ import numpy as np #instalado
 import struct
 import sys
 import wave 
-
-def is_valid(input):
-	if input == '-d':
-		return True
-	elif input == '-c':
-		return True
-	elif input == '-h':
-		return True
-	return False
-
+import pickle
 
 diff_bool = run_bool = huff_bool = False
 
+def is_valid(input):
+	if input == '-d':
+		diff_bool = True
+		return True
+	elif input == '-c':
+		run_bool = True
+		return True
+	elif input == '-h':
+		huff_bool = True
+		return True
+	return False
+
+def get_diffs(input_channels, samps, chans):
+	diff_channels_out = np.empty((chans, samps), dtype=int)
+	for i in range(chans):
+		diff_channels_out[i][0] = input_channels[i][0]
+		for ii in range(1, samps):
+			diff_channels_out[i][ii] = input_channels[i][ii] - input_channels[i][ii - 1]
+	return diff_channels_out
+
+
+
 #tratando os argumentos de entrada
 wave_file = wave.open(sys.argv[-2], 'r')
+output_file = open(sys.argv[-1], 'w+b')
 # wave_raw_data = open(sys.argv[-2], 'r')
 argc = len(sys.argv)
+
 
 
 if argc <= 3:
@@ -36,13 +51,22 @@ for i in range(1, 1+compressions):
 chans = wave_file.getnchannels()
 samps = wave_file.getnframes()
 
+diff_channels = np.empty((chans, samps), dtype=int)
+
 s = wave_file.readframes(samps)
 wave_file.close()
 
 unpstr = '<{0}h'.format(samps*chans)
 x = list(struct.unpack(unpstr, s))
 
-left_channel = x[0::chans]
-right_channel = x[1::chans]
+for i in range(chans):
+	diff_channels[i] = np.array(x[i::chans]) 
+
+left_channel = np.array(x[0::chans]) 
+right_channel = np.array(x[1::chans])
+
+print diff_channels[0]
+diff_channels_out = get_diffs(diff_channels, samps, chans)
+print diff_channels_out[0]
 
 
